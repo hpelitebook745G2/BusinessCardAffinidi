2eae4712b682df6f8e13a863ebbb353f59b9612b
Earl Grant Reyes <ambiguous.exe4585@gmail.com>
2023-03-24 17:13:58 +0800

    Added reducers

---
+++ src/reducers/ModalReducer.ts
@@ -0,0 +1,33 @@
+import {ModalInitialStateType} from '@/models/ModalModel';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+
+export const modalInitialState: ModalInitialStateType = {
+  isVisible: false,
+  modalOptions: {
+    modalType: 'double' || 'single',
+    buttonType: '',
+    headerText: '',
+    contentText: '',
+    txtLeft: '',
+    txtRight: '',
+    txtBtn: '',
+  },
+};
+
+const modalSlice = createSlice({
+  name: 'modal',
+  initialState: modalInitialState,
+  reducers: {
+    showModal: (state, action: PayloadAction<ModalInitialStateType>) => {
+      const {isVisible, modalOptions} = action.payload;
+      state.isVisible = isVisible;
+      state.modalOptions = modalOptions;
+    },
+    dismissModal: () => modalInitialState,
+  },
+  extraReducers: builder => {},
+});
+
+export const {showModal, dismissModal} = modalSlice.actions;
+
+export default modalSlice.reducer;
